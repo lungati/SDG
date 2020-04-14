e8161c3f961a17f5aa133eac7177d7b06b7ee008
Samuel Olembo <samuel.olembo@twigafoods.com>
2020-04-14 22:30:07 +0300

    Challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,19 @@
-def estimator(data):
-  return data
+from collections import namedtuple
+
+
+def get_infections_by_requested_time(currently_infected, days):
+    return currently_infected * (2 ** (days // 3))
+
+
+def estimator(data, requested_time):
+    data = namedtuple(data)
+
+    impact = severeImpact = namedtuple()
+    impact.currentlyInfected = data.reportedCases * 10
+    impact.infectionsByRequestedTime = get_infections_by_requested_time(
+        impact.currentlyInfected, requested_time)
+    severeImpact.currentlyInfected = data.reportedCases * 50
+    severeImpact.infectionsByRequestedTime = get_infections_by_requested_time(
+        severeImpact.currentlyInfected, requested_time)
+
+    return dict(data=data, impact=impact, severeImpact=severeImpact)
